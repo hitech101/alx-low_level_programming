82eac04aeff7e6684bc05f13a1c43bd030e4a11d
Henry Iwuno <henryiwuno@gmail.com>
2022-09-11 05:37:28 -0700

    4p

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+
 /**
  * main - printing
  * 
@@ -6,7 +7,6 @@
  */
 int main (void)
 {
-  puts("\"Program:writing is like building a multilingual puzzle");
-	  return (0);
+     puts("\"Program:writing is like building a multilingual puzzle");
+     return (0);
 }
-^C
