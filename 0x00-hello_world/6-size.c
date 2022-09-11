fd37a5740653ac2580ae05a67d9cd0481b517283
Henry Iwuno <henryiwuno@gmail.com>
2022-09-11 07:09:34 -0700

    6s

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,19 +1,15 @@
 #include <stdio.h>
 
-
 /**
  * main - print the string in the put function
  * Description: using the main function
  * this program prints "Programming is like building a multilingual puzzle
  * Return: 0
- */
-
+ */ 
 int main(void)
-
 {
-
-  char c;
-  int i;
+ char c;
+ int i;
  long li;
  long long lli;
  float f;
