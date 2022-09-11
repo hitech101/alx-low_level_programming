4c264a2f52ae098084e8f502d27159b6efc92f9e
Henry Iwuno <henryiwuno@gmail.com>
2022-09-10 19:53:40 -0700

    4puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#!/bin/bash
+#include <stdio.h>
+/**
+ * main - printing
+ * 
+ * Return always ezro 
+ */
+int main void
+{
+  puts("\"Program:writing is like building a multilingual puzzle")
+	  return (0);
+}
