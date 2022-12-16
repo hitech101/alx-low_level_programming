3b7df9a6e4f5e3b009cf0a19f89cca8a1a6c6392
Henry Iwuno <henryiwuno@gmail.com>
2022-12-15 17:01:11 -0800

    0-isupper.c

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+* _isupper  - a function that converts uppercase character
+* @c : An input character
+* Return: 1 if C is uppercase or 0 if otherwise
+*/
+int _isupport(int c)
+{
+char uppercase = "A";
+int isupper = "O"
+for (; uppercase <= "Z"; uppercase++)
+{
+if (c == uppercase)
+{
+isupper = 0;
+break;
+}
+}
+return (isupper);
+}
+
