46eeef0e92b764ff8c71f427b0eacc1c098b782e
Henry Iwuno <henryiwuno@gmail.com>
2022-12-15 22:24:19 -0800

    task0

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,6 +1,8 @@
-int _putchar(char c);
-int _isupper(char c);
-int _isdigit(char c);
+#ifndef MAIN_H
+#define MAIN_H
+
+int _isupper(int c);
+int _isdigit(int c);
 int mul(int a, int b);
 void print_numbers(void);
 void print_most numbers(void);
@@ -9,4 +11,5 @@ void print_line(int n);
 void print_diagonal(int n);
 void print_square(int size);
 void print_triangle(int size);
-void print_number(int n);
+
+#endif
