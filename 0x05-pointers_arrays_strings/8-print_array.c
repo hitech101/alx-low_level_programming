5ed39627c0c716e04941d82ec0ceb9b842727bd5
Henry Iwuno <henryiwuno@gmail.com>
2022-12-19 04:24:17 -0800

    t8

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -11,6 +11,7 @@
 void print_array(int *a, int n)
 {
 	int inc;
+
 	for (inc = 0; inc < n ; inc++)
 	{
 
@@ -18,8 +19,6 @@ void print_array(int *a, int n)
 			printf("%d, ", a[inc]);
 		else
 			printf("%d", a[inc]);
-		
 	}
 	putchar(10);
-
 }
