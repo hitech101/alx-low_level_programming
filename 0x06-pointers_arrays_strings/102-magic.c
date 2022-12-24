267233a6d075e133c950bf365351bc89e86be9a5
Henry Iwuno <henryiwuno@gmail.com>
2022-12-23 18:14:57 -0800

    t10

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -1,5 +1,10 @@
 #include <stdio.h>
 
+/**
+* main - void
+* Return:void
+*/
+
 int main(void)
 {
 	int n;
@@ -15,10 +20,10 @@ int main(void)
 * - you are not allowed to use a
 * - you are not allowed to modify p
 * - only one statement
-* - you are not allowed to code anything else than this line of code 
+* - you are not allowed to code anything else than this line of code
 */
-#*(p + 5) = 98;
+*(p + 5) = 98;
 /* ...so that this prints 98\n */
-	printf("a[2] = %r
-	return (0)
+	printf("a[2] = %d\", a[2]};
+	return(0)
 }
