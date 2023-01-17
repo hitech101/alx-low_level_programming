9c49c8fc933f638a43c5799c4c20a589e8d9f86c
Henry Iwuno <henryiwuno@gmail.com>
2023-01-16 20:12:14 -0800

    task 0

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,21 +1,22 @@
 #ifndef DOG_H
 #define DOG_H
+
 /**
- *  * struct dog - A new type describing a dog.
- *   * @name: The name of the dog.
- *    * @age: The age of the dog.
- *     * @owner: The owner of the dog
+ * struct dog - A new type describing a dog.
+ * @name: The name of the dog.
+ * @age: The age of the dog.
+ * @owner: The owner of the dog
  */
 struct dog
 {
-		char *name;
-			float age;
-				char *owner;
+	char *name;
+	float age;
+	char *owner;
 };
 
 /**
-*  * dog_t - Typedef for struct dog
-*/
+ * dog_t - Typedef for struct dog
+ */
 typedef struct dog dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
