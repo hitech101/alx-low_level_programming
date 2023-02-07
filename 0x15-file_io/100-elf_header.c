af0662697988f8a50d6fcaf52b164ab13b4aab1c
Henry Iwuno <henryiwuno@gmail.com>
2023-02-06 21:26:42 -0800

    task 2

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -177,7 +177,7 @@ void print_osabi(unsigned char *e_ident)
 void print_abi(unsigned char *e_ident)
 {
 	printf("  ABI Version:                       %d\n",
-		       	e_ident[EI_ABIVERSION]);
+			e_ident[EI_ABIVERSION]);
 }
 /**
  * print_type - Prints the type of an ELF header.
@@ -261,7 +261,9 @@ int main(int __attribute__((__unused__)) argc, char *argv[])
 {
 	Elf64_Ehdr *header;
 	int o, r;
+
 	o = open(argv[1], O_RDONLY);
+
 	if (o == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't read file %s\n", argv[1]);
